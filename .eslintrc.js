ed78be5e2eeee2711e981322e7b2f652797dc7a1
Djess-V <e.ageevets@yandex.ru>
2023-04-26 14:00:14 +0700

    environment setting

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: [
+    "airbnb-base",
+    "plugin:jest/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "import/prefer-default-export": "off",
+    "no-console": "off",
+    "no-restricted-globals": "off",
+    "no-restricted-syntax": "off",
+    "import/no-unresolved": "off",
+    "import/extensions": ["warn", "never"],
+    "@typescript-eslint/no-var-requires": "off",
+    "no-void": "off",
+    "no-extend-native": ["error", { exceptions: ["Function"] }],
+    "one-var": "off",
+    "no-else-return": "off",
+    "no-promise-executor-return": "off",
+    "arrow-body-style": "off",
+    "no-param-reassign": "off",
+    "no-use-before-define": "off",
+  },
+};
